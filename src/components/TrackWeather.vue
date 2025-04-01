27cd2887d421d6a811f45a4338b71968747a6878
Réka Hajdú <hajdu.reka.009@gmail.com>
2025-04-01 18:43:01 +0200

    TrackWeather component and API request

---
+++ src/components/TrackWeather.vue
@@ -0,0 +1,45 @@
+<script setup>
+	import { ref, onMounted, computed } from "vue";
+
+	import tracks from "@/data/tracks.json";
+
+	const now = new Date();
+
+	// Find next race based on date
+	const nextRaceIndex = tracks.findIndex((track) => new Date(track.date) > now);
+	const trackIndex = ref(nextRaceIndex !== -1 ? nextRaceIndex : 0);
+	const selectedTrack = computed(() => tracks[trackIndex.value]);
+	const weatherData = ref(null);
+
+	const fetchWeather = async () => {
+		const { lat, long } = selectedTrack.value.location;
+
+		const res = await fetch(
+			`https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${long}&exclude=minutely,hourly,daily,alerts&appid=${
+				import.meta.env.VITE_WEATHER_API_KEY
+			}&units=metric`
+		);
+
+		const data = await res.json();
+		weatherData.value = data;
+	};
+
+	onMounted(() => {
+		fetchWeather();
+	});
+</script>
+<template>
+	<div v-if="weatherData && weatherData.current">
+		<h2>{{ selectedTrack.raceName }}</h2>
+		<p>Track: {{ selectedTrack.circuitName }}</p>
+		<p>Date: {{ selectedTrack.date }}</p>
+
+		<p>Temp: {{ weatherData.current.temp }} °C</p>
+		<p>Condition: {{ weatherData.current.weather[0].description }}</p>
+		<p>UV Index: {{ weatherData.current.uvi }}</p>
+	</div>
+
+	<div v-else>
+		<p>Loading weather data...</p>
+	</div>
+</template>
