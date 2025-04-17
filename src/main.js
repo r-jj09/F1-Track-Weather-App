0accf4e53d4d68c16d549ec317a5731c1481db30
Réka Hajdú <hajdu.reka.009@gmail.com>
2025-04-17 12:57:36 +0200

    Add preloader component and improve loading state management in App.vue

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import "./style.css";
+import "./loader.css";
 
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
